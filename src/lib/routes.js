2f7833825ee6523893dfd2b44ce97c054a01fbb3
George <71434803+george-swift@users.noreply.github.com>
2023-12-14 16:28:20 +0100

    Define main routes

---
+++ src/lib/routes.js
@@ -0,0 +1,11 @@
+export const Routes = {
+  LOGIN: '/login',
+  HOME: '/',
+  ARTISTS: '/artists',
+  ARTIST: '/artist',
+  TRACKS: '/tracks',
+  TRACK: '/track',
+  RECENTLY_PLAYED: '/recent',
+  PLAYLISTS: '/playlists',
+  RECOMMENDATIONS: '/recommendations'
+}
